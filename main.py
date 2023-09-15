308e82fecb3e5715b1db6ecac8250e84bfa0d45c
STUTuna <df95111111@gmail.com>
2023-09-15 14:42:40 +0800

    style: 修改程式碼排版和增加空行

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ for developer in developers:
     github_links.append((name, github_link))
 
 # 輸出每個開發者的名稱和 GitHub 連結 並計算開發者的數量
+print('---------------------------------')
+print('各別Github連結')
 for name, link in github_links:
     print(f'{name}: {link}')
 print(f'Total developers: {len(github_links)}')
@@ -32,7 +34,8 @@ print(f'Total developers: {len(github_links)}')
 # 訪問每個開發者的 GitHub 頁面
 # 查詢有沒有網頁元素包含開發者的位置個人網站
 # 如果有，則輸出開發者的名稱和位置網站
-
+print('---------------------------------')
+print('開發者的個人網站')
 for name, link in github_links:
     response = requests.get(link)
     soup = BeautifulSoup(response.content, 'html.parser')
